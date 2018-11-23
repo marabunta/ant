52663c1834372a8babac907e492e75bd273df5c9
nbari <nbari@tequila.io>
2018-11-23 21:01:56 +0100

    sync Fri Nov 23 21:01:56 CET 2018

--- cmd/ant/main.go
+++ cmd/ant/main.go
@@ -1,44 +1,72 @@
 package main
 
 import (
-	"flag"
+	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
-	"os"
+	"io/ioutil"
+	"log"
 
-	"github.com/marabunta/client"
+	pb "github.com/marabunta/protobuf"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
-var version string
+//var version string
 
 func main() {
-	var (
-		v        = flag.Bool("v", false, fmt.Sprintf("Print version: %s", version))
-		id       = flag.String("id", "", "ant ID")
-		host     = flag.String("host", "", "Connect to `host` default (marabunta)")
-		port     = flag.Int("port", 1415, "Port number to use for the connection")
-		certFile = flag.String("cert", "server.crt", "TLS cert")
+	//var (
+	//v        = flag.Bool("v", false, fmt.Sprintf("Print version: %s", version))
+	//id       = flag.String("id", "", "ant ID")
+	//host     = flag.String("host", "", "Connect to `host` default (marabunta)")
+	//port     = flag.Int("port", 1415, "Port number to use for the connection")
+	//certFile = flag.String("cert", "server.crt", "TLS cert")
+	//)
+
+	//flag.Parse()
+	//if *v {
+	//fmt.Printf("%s\n", version)
+	//os.Exit(0)
+	//}
+
+	certificate, err := tls.LoadX509KeyPair(
+		"./certs/client.crt",
+		"./certs/client.key",
 	)
 
-	flag.Parse()
-	if *v {
-		fmt.Printf("%s\n", version)
-		os.Exit(0)
-	}
-
-	if _, err := os.Stat(*certFile); os.IsNotExist(err) {
-		fmt.Fprintf(os.Stderr, "Cannot read the server certificate file: %s, use -h for more info.\n", *certFile)
-		os.Exit(1)
-	}
-
-	ant, err := client.New(*id, *host, *port)
+	certPool := x509.NewCertPool()
+	bs, err := ioutil.ReadFile("./certs/CA.crt")
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		log.Fatalf("failed to read ca cert: %s", err)
 	}
 
-	// Run it
-	if err := ant.Run(*certFile); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+	ok := certPool.AppendCertsFromPEM(bs)
+	if !ok {
+		log.Fatal("failed to append certs")
+	}
+
+	transportCreds := credentials.NewTLS(&tls.Config{
+		ServerName:   "marabunta.host",
+		Certificates: []tls.Certificate{certificate},
+		RootCAs:      certPool,
+	})
+
+	dialOption := grpc.WithTransportCredentials(transportCreds)
+	conn, err := grpc.Dial("localhost:1415", dialOption)
+	if err != nil {
+		log.Fatalf("failed to dial server: %s", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewMarabuntaClient(conn)
+
+	stream, err := client.Stream(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("stream = %+v\n", stream)
+	for {
+
 	}
 }
